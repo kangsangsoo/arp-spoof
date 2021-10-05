d20c818ae3de297d095b7746bcf4c900fac6590d
kangsangsoo <63638850+kangsangsoo@users.noreply.github.com>
2021-10-05 23:13:46 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "send-arp.h"
 
+
+
 int main(int argc, char* argv[]) {
+	void (*signal_hand)(int);
+	signal_hand = signal(SIGINT, sigint_handler);
 
 	// 입력 인자 개수가 4개 이상이어야 하며 짝수여야함.
 	if (argc < 4 || argc % 2 != 0) {
@@ -15,8 +19,6 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
 		return -1;
 	}
-
-	
 	
 	// unordered_map vs map
 	std::map<Ip, Mac> ARPtable;
@@ -32,85 +34,64 @@ int main(int argc, char* argv[]) {
 		initArg(&argv[now], sender, target);
 		IpTable.push_back({sender, target});
 		// sender랑 target Mac 찾기
-		if(resolveMac(handle, ARPtable, me, sender) == FAIL);
-		if(resolveMac(handle, ARPtable, me, target) == FAIL);
+		// 휴대폰 화면 꺼져있으면 Mac주소 못찾음..
+		if(resolveMac(handle, ARPtable, me, sender) == FAIL) return 0;
+		if(resolveMac(handle, ARPtable, me, target) == FAIL) return 0;
 
-		//printTable(ARPtable);
-
-		// 감염 시키는건데 
-
-		/*
-		// me, sender, target의 Ip 구조체를 넘겨주고 맥주소는 table에서 알아서 찾도록
-		cout << "go to trehad" << endl;
-		std::thread t(infection, handle, ARPtable, me, sender, target); 
-		t.join();
-		*/
 		now = now + 2;
 	}
 
+	// 디버깅용
 	for(auto i = ARPtable.begin(); i != ARPtable.end(); i++) {
 		cout << string(i->first) << ' ' << uint32_t(i->first) << ' ' << string(i->second) << endl;
 	}
+	for(auto i = IpTable.begin(); i != IpTable.end(); i++) {
+		cout << string(i->first) << ' '  << string(i->second) << endl;
+	}
+	//
+	std::thread::native_handle_type t_set[100];
 
 	// infection 실행
 	cout << "infection start" << endl;
 	for(int i = 0; i < IpTable.size(); i++) {
-		std::thread t(infection, handle, ARPtable, me, IpTable[i].first, IpTable[i].second, 10, 1);
-		t.detach();
+		t_set[i] = infection(handle, ARPtable, me, IpTable[i].first, IpTable[i].second, 5, 1);
 	}
-
 
 	// watchPacket으로
 	// IP 패킷일 경우 => flow로 
 	// ARP 패킷일 경우 => 감염으로 대응해줘야 함
 	// 1초당 1번씩 보내고 있을 때 그닥 걱정은 안됨
 	// 더 큰 주기로 보낸다 했을 때 ARP Table 복구를 염두해야함.
-
-	watchPacket(handle, ARPtable, IpTable, me);
-
-
-
+	// 종료는 ctrl + c로 signal 보내서 가능
+	watchPacket(handle, ARPtable, IpTable, me); // while 조건문 1로 해둠
 	
 	// 10초 대기
-	auto startTime = std::chrono::system_clock::now();
-	while (1) {
-		auto endTime = std::chrono::system_clock::now();
-		auto diff = std::chrono::duration_cast<std::chrono::seconds>(endTime-startTime);
-		if(diff.count() > 10) {
-			break;
-		}
-	}
+	// auto startTime = std::chrono::system_clock::now();
+	// while (1) {
+	// 	auto endTime = std::chrono::system_clock::now();
+	// 	auto diff = std::chrono::duration_cast<std::chrono::seconds>(endTime-startTime);
+	// 	if(diff.count() > 10) {
+	// 		break;
+	// 	}
+	// }
 
-
-
-
-	// infection말고 recover를 실행
-	// void infection(pcap_t* handle, map<Ip, Mac> table, Ip me, Ip sender, Ip target, Ip times, Ip sec) 
-	// void recover(pcap_t* handle, map<Ip, Mac>& table, Ip& sender, Ip& target) {
-	findFlag = 1;
 	cout << "infection end" << endl;
 	
-	// 2초 대기
-	startTime = std::chrono::system_clock::now();
-	while (1) {
-		auto endTime = std::chrono::system_clock::now();
-		auto diff = std::chrono::duration_cast<std::chrono::seconds>(endTime-startTime);
-		if(diff.count() > 2) {
-			break;
-		}
+
+	for(int i = 0; i < IpTable.size(); i++) {
+		pthread_cancel(t_set[i]);
+		pthread_join(t_set[i], NULL);
 	}
 
 	cout << "recover start" << endl;
 	findFlag = 0;
-	std::thread t_[10];
 
 	for(int i = 0; i < IpTable.size(); i++) {
-		t_[i] = std::thread(recover, handle, ARPtable, IpTable[i].first, IpTable[i].second);
+		recover(handle, ARPtable, IpTable[i].first, IpTable[i].second);
 	}
-	for(int i = 0; i < IpTable.size(); i++) {
-		t_[i].join();
-	}
+
 	cout << "program end" << endl;
+	// handle이 먼저 종료되면 기존에 handle로 처리한더거 처리 못함
 	pcap_close(handle);
 	return 0;
 }
