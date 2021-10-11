1d5dd07963c663b8558b59745bbb78eaf566788c
kangsangsoo <63638850+kangsangsoo@users.noreply.github.com>
2021-10-11 18:35:44 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,30 +32,52 @@ int main(int argc, char* argv[]) {
 	std::vector <pair<Ip, Ip>> IpTable; // {sender, target}
 	while(now < argc) {
 		initArg(&argv[now], sender, target);
+
+
+		// 사용자가 입력을 이상하게 했을 경우?? 고려해야하나?
+		// 동일한 {sender, target} 쌍이 들어온다면?
+		// vector에서 O(n)으로 서치하자.
+		int dup_flag = 0;
+		for(auto i : IpTable) {
+			if(i.first == sender && i.second == target) {
+				dup_flag = 1;
+				#ifdef DEBUG
+				cout << "dup find" << endl;
+				#endif
+				now = now + 2;
+				break;
+			}
+
+		}
+		if(dup_flag) continue;
+
+
 		IpTable.push_back({sender, target});
 		// sender랑 target Mac 찾기
 		// 휴대폰 화면 꺼져있으면 Mac주소 못찾음..
+
 		if(resolveMac(handle, ARPtable, me, sender) == FAIL) return 0;
 		if(resolveMac(handle, ARPtable, me, target) == FAIL) return 0;
 
 		now = now + 2;
 	}
 
-	// 디버깅용
+	#ifdef DEBUG
 	for(auto i = ARPtable.begin(); i != ARPtable.end(); i++) {
 		cout << string(i->first) << ' ' << uint32_t(i->first) << ' ' << string(i->second) << endl;
 	}
 	for(auto i = IpTable.begin(); i != IpTable.end(); i++) {
 		cout << string(i->first) << ' '  << string(i->second) << endl;
 	}
+	cout << "infection start" << endl;
+	#endif
 
 	// infection 실행
-	cout << "infection start" << endl;
 	for(auto i : IpTable) {
 		infectionPacket.push_back(fillPacket(ARPtable[me], ARPtable[i.first], ARPtable[me], i.second, ARPtable[i.first], i.first, ArpHdr::Reply));
 	}
 	pthread_t t;
-	int tid = pthread_create(&t, NULL, infection, (void*)handle);
+	int rs = pthread_create(&t, NULL, infection, (void*)handle);
 
 
 
@@ -67,13 +89,12 @@ int main(int argc, char* argv[]) {
 	// 종료는 ctrl + c로 signal 보내서 가능
 	watchPacket(handle, ARPtable, IpTable, me); // while 조건문 1로 해둠
 	
-
+	#ifdef DEBUG
 	cout << "infection end" << endl;
-	
-	pthread_join(tid, NULL);
-
+	rs = pthread_join(t, NULL);
+	cout << rs << endl;
 	cout << "recover start" << endl;
-
+	#endif
 
 
 	for(auto i : IpTable) {
@@ -81,7 +102,7 @@ int main(int argc, char* argv[]) {
 	}
 
 
-	cout << "program end" << endl;
+	cout << "exit" << endl;
 	// handle이 먼저 종료되면 기존에 handle로 처리한더거 처리 못함
 	pcap_close(handle);
 	return 0;
